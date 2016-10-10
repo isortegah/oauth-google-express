2546efd914fd3340178258cef0ba3625f905beef
IORTREGA <iortega@nearbpo.com>
2016-10-09 21:03:21 -0500

    Express
    Configuracion inicial para servidor con express

---
+++ app.js
@@ -0,0 +1,13 @@
+ /**
+ * Created by ISORTEGAH on 09/10/2016
+ */
+ var express = require('express');
+ var app     = express();
+ var http    = require('http');
+ var port    = process.env.PORT || 3000;
+ 
+ var server = http.createServer(app);
+ server.listen(port)
+ console.log("\n//////////////////////////////////////////////");
+ console.log("////// http server listening on %d /////////", port);
+ console.log("//////////////////////////////////////////////");
